cb45dd68b61d8dcf9db88115a000f7eed3835f69
Kevin Feng <fengfengkevin@gmail.com>
2019-10-14 23:40:53 -0400

    Updates

---
+++ precache-manifest.f4c2cbc3b111eb5fd7c4a0e115a58b64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9fa9e8ff7ed9850f0916e1aff42ab8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1074c436f34b1a53e80f",
+    "url": "/calculator/static/css/main.29c73c12.chunk.css"
+  },
+  {
+    "revision": "2c91933479d79d9c8ba3",
+    "url": "/calculator/static/js/2.3f3fd1c7.chunk.js"
+  },
+  {
+    "revision": "1074c436f34b1a53e80f",
+    "url": "/calculator/static/js/main.5f00adc0.chunk.js"
+  },
+  {
+    "revision": "4ed1cf36ed9e5a064592",
+    "url": "/calculator/static/js/runtime-main.b8dd65a4.js"
+  }
+]);
